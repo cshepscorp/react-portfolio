9abd22b7580a06d87a51f58293e27dae70a9ff6d
cshepscorp <sheppard.christy@gmail.com>
2022-01-14 17:08:40 -0500

    spa achieved

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+
+function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
+  const [resumeSelected, setResumeSelected] = useState(false);
+  const [aboutSelected, setAboutSelected] = useState(false); // prevent the contact form from showing when a user initially navigates to the homepage
+
+  return (
+    <div>
+      <Nav
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+        portfolioSelected={portfolioSelected}
+        setPortfolioSelected={setPortfolioSelected}
+        resumeSelected={resumeSelected}
+        setResumeSelected={setResumeSelected}
+        aboutSelected={aboutSelected}
+        setAboutSelected={setAboutSelected}
+      ></Nav>
+      <main>
+        {!contactSelected && !portfolioSelected && !resumeSelected ? (
+          <>
+            <Hero></Hero>
+            <About></About>
+          </>
+        ) : (
+          <></>
+        )}
+        {contactSelected ? <Contact></Contact> : <></>}
+        {portfolioSelected ? <Portfolio></Portfolio> : <></>}
+        {resumeSelected ? <Resume></Resume> : <></>}
+      </main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Nav(props) {
+  const { contactSelected, setContactSelected } = props;
+  const { portfolioSelected, setPortfolioSelected } = props;
+  const { resumeSelected, setResumeSelected } = props;
+  const { aboutSelected, setAboutSelected } = props;
+  return (
+    <header>
+      {/* <!-- site logo graphic and or main text logo --> */}
+      <h1 onClick={() => { setContactSelected(false);
+                            setAboutSelected(false);
+                            setResumeSelected(false);
+                             setPortfolioSelected(false)}}>
+        <a>Christy Sheppard</a>
+      </h1>
+      {/* <!-- site navigation --> */}
+      <nav>
+        <ul>
+          <li onClick={() => { setContactSelected(false);
+                                      setAboutSelected(true);
+                            setResumeSelected(false);
+                             setPortfolioSelected(false)}}>
+            <a>About Me</a>
+          </li>
+        </ul>
+        <ul>
+        <li onClick={() => { setContactSelected(false);
+                                    setAboutSelected(false);
+                              setResumeSelected(false);
+                             setPortfolioSelected(true)}}>
+            <a>Portfolio</a>
+          </li>
+        </ul>
+        <ul>
+        <li onClick={() => { setContactSelected(false);
+                                    setAboutSelected(false);
+                              setResumeSelected(true);   
+                             setPortfolioSelected(false)}}>
+            <a>Resume</a>
+          </li>
+        </ul>
+        <ul>
+          <li
+            className={`${contactSelected && "navActive"}`}
+            onClick={() => { setContactSelected(true);
+              setAboutSelected(false);
+              setResumeSelected(false);   
+             setPortfolioSelected(false)}}>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
