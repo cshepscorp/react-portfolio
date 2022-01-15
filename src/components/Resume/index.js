313ea9ead1b8fb6075d24c75311ce79da8184e28
cshepscorp <sheppard.christy@gmail.com>
2022-01-15 14:33:36 -0500

    resume details filled out

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,21 +1,42 @@
 import React from "react";
-
 function Resume() {
   return (
     <section id="about">
-    <div class="intro">
-      <h3>Resume</h3>
-    </div>
-    <div class="guts">
-      <h4>Hi, I'm Christy</h4>
-      <p>
-        I love a challenge. When I'm tasked with a project I'm not 100% sure
-        how to complete - I get excited - and then I get to work! I would love
-        the opportunity to put that enthusiasm into action for you, and for us
-        to end up with something beautiful as a result.
-      </p>
-    </div>
-  </section>
+      <div class="intro">
+        <h3>Resume</h3>
+      </div>
+      <div class="guts">
+        <div class="resume">
+        <h3>
+          <a href="https://github.com/cshepscorp/react-portfolio/blob/main/public/Resume-Christy-Sheppard.pdf">
+            Download
+          </a>{" "}
+          my Resume
+        </h3>
+          <h3>Technical Skills</h3>
+          <h4>Frontend:</h4>
+          <ul>
+            <li>React</li>
+            <li>JQuery</li>
+            <li>Bootstrap</li>
+            <li>CSS</li>
+            <li>Handlebars</li>
+            <li>WordPress</li>
+            <li>Shopify</li>
+          </ul>
+          <h4>Backend:</h4>
+          <ul>
+            <li>MySQL</li>
+            <li>Express</li>
+            <li>Node-js</li>
+            <li>Sequelize</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>IndexedDB</li>
+          </ul>
+          </div>
+      </div>
+    </section>
   );
 }
 
