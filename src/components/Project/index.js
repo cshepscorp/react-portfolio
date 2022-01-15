2006ad58b9e522e9cb43967547291db4ae89815e
cshepscorp <sheppard.christy@gmail.com>
2022-01-15 16:43:59 -0500

    minor style fix

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,13 +3,19 @@ import React from "react";
 function Project(props) {
   return (
     <div class="card" backgroundImage>
-      <img alt={props.name} src={props.image} width="100%" />
+      <a href={props.appLink}>
+        <img alt={props.name} src={props.image} width="100%" />
+      </a>
       <h3>{props.name}</h3>
       <p>
-        <a href={props.appLink}>Deployed App</a>
+        <a href={props.appLink}>
+          <i class="fa fa-external-link"></i> Deployed App
+        </a>
       </p>
       <p>
-        <a href={props.gitHubLink}>GitHub Repo</a>
+        <a href={props.gitHubLink}>
+          <i class="fa fa-github-square"></i> GitHub Repo
+        </a>
       </p>
     </div>
   );
