52d96d44aba14f3cc9e9e96c0a0b7732c5e6f81d
cshepscorp <sheppard.christy@gmail.com>
2022-01-15 11:53:01 -0500

    fixed formatting on resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,10 +2,20 @@ import React from "react";
 
 function Resume() {
   return (
-    <div>
-      RESUME CONTENT
-      I see a link to a downloadable resume and a list of the developer’s proficiencies
+    <section id="about">
+    <div class="intro">
+      <h3>Resume</h3>
     </div>
+    <div class="guts">
+      <h4>Hi, I'm Christy</h4>
+      <p>
+        I love a challenge. When I'm tasked with a project I'm not 100% sure
+        how to complete - I get excited - and then I get to work! I would love
+        the opportunity to put that enthusiasm into action for you, and for us
+        to end up with something beautiful as a result.
+      </p>
+    </div>
+  </section>
   );
 }
 
