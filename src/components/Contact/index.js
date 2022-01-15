bfd3d45a48c5c2904ab0aeb1fa5f0088e1ef32ea
cshepscorp <sheppard.christy@gmail.com>
2022-01-15 11:20:24 -0500

    contact form validated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,47 +1,91 @@
 import React, { useState } from "react";
-
+import { validateEmail } from "../../utils/helpers";
 
 // The name of this function isn't critical and doesn't have to match the name of the component folder
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' }); // set forms to blank upon component load
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  }); // set forms to blank upon component load
 
   const { name, email, message } = formState; // deconstructed
   // sync the internal state of the component formState with the user input from the DOM
+
+  // use the useState Hook to handle the error state
+  const [errorMessage, setErrorMessage] = useState("");
+
   function handleChange(e) {
-      setFormState({...formState, [e.target.name]: e.target.value }) // name in e.target.name refers to the name attr of the form element. This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is a required field.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value }); // name in e.target.name refers to the name attr of the form element. This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names
+    }
   }
 
   function handleSubmit(e) {
-      e.preventDefault();
-      console.log(formState);
+    e.preventDefault();
+    console.log(formState);
   }
 
   //console.log(formState);
 
   return (
     <section id="contact">
-        <div class="intro">
-            <h3>
-                Contact Me
-            </h3>
-        </div>
-        <div class="guts">
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </div> 
+      <div class="intro">
+        <h3>Contact Me</h3>
+      </div>
+      <div class="guts">
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              defaultValue={name}
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email address:</label>
+            <input
+              type="email"
+              name="email"
+              defaultValue={email}
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              name="message"
+              rows="5"
+              defaultValue={message}
+              onBlur={handleChange}
+            />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </section>
   );
 }
---
+++ src/utils/helpers.js
@@ -0,0 +1,5 @@
+export function validateEmail(email) {
+  var regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regEx.test(String(email).toLowerCase());
+}
