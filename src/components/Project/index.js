430acad25626b52938ccd88f487585058e97a922
cshepscorp <sheppard.christy@gmail.com>
2022-01-15 13:20:58 -0500

    projects loading in portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,12 +2,16 @@ import React from "react";
 
 function Project(props) {
   return (
-      <div class="card">
-          <img alt={props.name} src={props.image} />
-          <h4>{props.name}</h4>
-          <p><a href={props.appLink}>Deployed App</a></p>
-          <p><a href={props.gitHubLink}>GitHub Repo</a></p>
-      </div>
+    <div class="card" backgroundImage>
+      <img alt={props.name} src={props.image} width="100%" height="180px" />
+      <h3>{props.name}</h3>
+      <p>
+        <a href={props.appLink}>Deployed App</a>
+      </p>
+      <p>
+        <a href={props.gitHubLink}>GitHub Repo</a>
+      </p>
+    </div>
   );
 }
 
