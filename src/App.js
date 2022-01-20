a72a63d00e07f8d6b8df51e7225dfe686adbbc8d
cshepscorp <sheppard.christy@gmail.com>
2022-01-20 14:20:19 -0500

    using react router to serve pages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
+// react router
+// BrowserRouter and Route are components that the React Router library provides. We renamed BrowserRouter to Router to make it easier to work with.
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from "./components/Nav";
 import About from "./components/About";
-import Hero from "./components/Hero";
 import Portfolio from "./components/Portfolio";
 import ContactForm from "./components/Contact";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
-  const [portfolioSelected, setPortfolioSelected] = useState(false);
-  const [resumeSelected, setResumeSelected] = useState(false);
-  const [aboutSelected, setAboutSelected] = useState(true); // prevent the contact form from showing when a user initially navigates to the homepage
 
   return (
     <div>
-      <Nav
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-        portfolioSelected={portfolioSelected}
-        setPortfolioSelected={setPortfolioSelected}
-        resumeSelected={resumeSelected}
-        setResumeSelected={setResumeSelected}
-        aboutSelected={aboutSelected}
-        setAboutSelected={setAboutSelected}
-      ></Nav>
-      <main>
-        {!contactSelected && !portfolioSelected && !resumeSelected ? (
-          <>
-            <Hero></Hero>
-            <About></About>
-          </>
-        ) : (
-          <></>
-        )}
-        {contactSelected ? <ContactForm></ContactForm> : <></>}
-        {portfolioSelected ? <Portfolio></Portfolio> : <></>}
-        {resumeSelected ? <Resume></Resume> : <></>}
-      </main>
-      <Footer></Footer>
+      <Router>
+        <Nav />
+        <main>
+          <div className="container">
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/resume" component={Resume} />
+            <Route exact path="/Contact" component={ContactForm} />
+          </div>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
