b1accc07ad2affc2dd4f6a4a15470d407e4a0c34
cshepscorp <sheppard.christy@gmail.com>
2022-01-20 14:33:21 -0500

    image route revised

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
         <h3>About Me</h3>
       </div>
       <div class="guts">
-        <img src="https://github.com/cshepscorp/react-portfolio/blob/main/public/images/hero-headshot-vinyls.png" />
+        <img src="https://raw.githubusercontent.com/cshepscorp/react-portfolio/main/public/images/hero-headshot-vinyls.png" />
         <h4>Hi, My name Christy Sheppard</h4>
         <p>
           I am a full-stack web developer utilizing deep experience with Adobe
