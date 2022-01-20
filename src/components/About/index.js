a398e1c0749cd360f4efb784eecaaa3754ab0f12
cshepscorp <sheppard.christy@gmail.com>
2022-01-20 16:20:58 -0500

    updated README

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,20 +7,19 @@ function About() {
         <h3>About Me</h3>
       </div>
       <div className="headshot">
-      <img
+        <img
           src="https://raw.githubusercontent.com/cshepscorp/react-portfolio/main/public/images/hero-headshot-vinyls.png"
           alt="Christina Sheppard headshot"
           width="400px"
         />
       </div>
       <div className="guts">
-        
         <h4>Hi, I'm Christy Sheppard</h4>
         <p>
-          I am a Full Stack Web Developer utilizing deep experience with Adobe
-          Graphic Suite and e-commerce management skills to craft and design
-          attractive and intuitive user experiences on the web. I've been in the
-          workforce for a long time working in front-end web
+          I am a Full Stack Web Developer utilizing a decade of experience with
+          Adobe Graphic Suite and e-commerce management skills to craft and
+          design attractive and intuitive user experiences on the web. I've been
+          in the workforce for a long time working in front-end web
           development/management and recently decided I'd like to get to know
           all about how the back-end works, too. When I'm tasked with a project
           I'm not 100% sure how to complete - I get excited - and then I get to
