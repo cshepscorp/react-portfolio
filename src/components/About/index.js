a853f756dce4084505a633ae2dcb3da11f30164f
cshepscorp <sheppard.christy@gmail.com>
2022-01-20 13:53:36 -0500

    minor update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,11 @@ function About() {
       <div class="guts">
         <h4>Hi, My name Christy Sheppard</h4>
         <p>
-        I am a front-end web developer utilizing deep experience with Adobe Graphic Suite and e-commerce management skills to craft and design attractive and intuitive user experiences on the web. I love a challenge. When I'm tasked with a project I'm not 100% sure how to complete - I get excited - and then I get to work!
+          I am a front-end web developer utilizing deep experience with Adobe
+          Graphic Suite and e-commerce management skills to craft and design
+          attractive and intuitive user experiences on the web. I love a
+          challenge. When I'm tasked with a project I'm not 100% sure how to
+          complete - I get excited - and then I get to work!
         </p>
       </div>
     </section>
