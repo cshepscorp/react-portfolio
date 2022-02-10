669e23665d4be516b3289502a6e2bba69fe5e00c
cshepscorp <sheppard.christy@gmail.com>
2022-02-10 09:59:16 -0500

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,14 +15,14 @@ function About() {
       <div className="guts">
         <h4>Hi, I'm Christy Sheppard</h4>
         <p>
-          I am a Full Stack Web Developer utilizing a decade of experience with
-          Adobe Graphic Suite and e-commerce management skills to craft and
-          design attractive and intuitive user experiences on the web. I've been
-          in the workforce for a long time working in front-end web
-          development/management and recently decided I'd like to get to know
-          all about how the back-end works, too. When I'm tasked with a project
-          I'm not 100% sure how to complete - I get excited - and then I get to
-          work!
+          I am a Full Stack Web Developer utilizing over a decade of experience
+          with Adobe Creative Suite and e-commerce management skills to craft
+          and design attractive and intuitive user experiences on the web. I've
+          been in the workforce for a long time working in front-end web content
+          management and basic development and recently decided I'd like to get
+          to know more about how things work behind the scenes, as well. When
+          I'm tasked with a project I'm not 100% sure how to complete - I get
+          excited - and then I get to work!
         </p>
       </div>
     </section>
